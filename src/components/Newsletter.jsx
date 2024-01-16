c64cc5e2869b4ccb853585b25494ef1d6ad3c080
Kerekesss <kristoferkerekes815@gmail.com>
2024-01-16 17:29:37 +0100

    first commit

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -11,13 +11,13 @@ const Newsletter = () => {
           <p className="py-2">Sign up to our newsletter and stay up to date.</p>
         </div>
         <div className="flex flex-col pt-2 ">
-          <div className="flex max-lg:flex-col gap-4 justify-center max-lg:px-4">
+          <div className="flex max-lg:flex-col gap-4 max-lg:items-center max-lg:px-4">
             <input
               type="text"
               placeholder="Enter your Email"
-              className="p-2 px-3 rounded-lg w-[250px] max-lg:w-[580px] max-md:w-[470px] max-sm:w-[390px]"
+              className="p-2 px-3 rounded-lg w-[250px] max-lg:w-[580px] max-md:w-[470px] max-sm:w-[90%]"
             />
-            <button className="bg-green1 text-black w-[130px] rounded-md font-medium py-3 max-lg:w-[580px] max-md:w-[470px] max-sm:w-[390px]">
+            <button className="bg-green1 text-black w-[130px] rounded-md font-medium py-3 max-lg:w-[580px] max-md:w-[470px] max-sm:w-[90%]">
               Notify me
             </button>
           </div>
